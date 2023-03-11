678c7673b6addf445c6a5f42f3ed3bf6f5bfe9f5
lucas.clementi <lucasclementi@gmail.com>
2023-03-11 13:24:29 -0700

    calculator with addition,subtraction,multiplication,division, and square root.

--- calculator/calc.js
+++ calculator/calc.js
@@ -13,7 +13,25 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
-
+	if (mathSymbol === "+") {
+			console.log(num1 +num2);
+	}
+	else if (mathSymbol === "-") {
+			console.log(num1 -num2);
+	}
+	else if (mathSymbol ==="*") {
+			console.log(num1 *num2);
+	}
+	else if (mathSymbol === "/") {
+			console.log(num1 /num2);
+	}
+	else if (mathSymbol ==="sqrt") {
+			console.log(Math.sqrt(num1))
+	}
+	else {
+		console.log("Invalid operation");
+	}
+	
 
 	// This line closes the connection to the command line interface.
 	reader.close()
